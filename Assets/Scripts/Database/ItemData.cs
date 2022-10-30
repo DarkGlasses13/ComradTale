152344a8cc4018f4a04965af7fe21f1a0b2779ba
Styopa Groznov <groznov3@gmail.com>
2022-10-30 09:06:48 +0300

    Discribed base classes

---
+++ Assets/Scripts/Database/ItemData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Database
+{
+    [Serializable]
+    public class ItemData
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public int Price;
+        public int Stack;
+
+        public ItemData(int id, string title, string description, int price, int stack)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Price = price;
+            Stack = stack;
+        }
+    }
+}
