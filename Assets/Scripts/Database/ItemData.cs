5f7b098723a1972ebb77f7e9ae31473c275e6d86
Styopa Groznov <groznov3@gmail.com>
2022-11-04 20:20:50 +0300

    Instal item database

--- Assets/Scripts/Database/ItemData.cs
+++ Assets/Scripts/Database/ItemData.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Database
 {
     [Serializable]
     public class ItemData
     {
-        public int Id;
-        public string Title;
-        public string Description;
-        public int Price;
-        public int Stack;
+        private List<Stat> _stats;
+        public string Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int Price { get; private set; }
+        public int Stack { get; private set; }
 
-        public ItemData(int id, string title, string description, int price, int stack)
+        [Newtonsoft.Json.JsonConstructor]
+        public ItemData(string title, string description, int price, int stack, List<Stat> stats)
         {
-            Id = id;
+            Id = $"it_{title}";
             Title = title;
             Description = description;
             Price = price;
             Stack = stack;
+            _stats = stats;
         }
+
+        public Stat GetStat(Stats title) => _stats.SingleOrDefault(stat => stat.Title.Equals(title.ToString()));
     }
 }
--- Assets/Scripts/Database/ItemDatabase.cs
+++ Assets/Scripts/Database/ItemDatabase.cs
@@ -1,40 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Database
 {
     public class ItemDatabase
     {
-        private List<ItemData> _items = new();
-        private readonly char _cellSeparator = ',';
-        private readonly char _inCellSeparator = ';';
+        private readonly List<ItemData> _items;
 
         public ItemDatabase()
         {
-            ProcessData(Resources.Load<TextAsset>("Database/ItemsSheet"));
-        }
-
-        private void ProcessData(TextAsset data)
-        {
-            int titleIndex = 0;
-            int descriptionIndex = 1;
-            int priceIndex = 2;
-            int stackIndex = 3;
-            string[] rows = data.text.Split('\n');
-            int dataRawIndex = 1;
-
-            for (int i = dataRawIndex; i < rows.Length; i++)
-            {
-                string[] cells = rows[i].Split(_cellSeparator);
-
-                string title = cells[titleIndex];
-                string description = cells[descriptionIndex];
-                int price = int.Parse(cells[priceIndex]);
-                int stack = int.Parse(cells[stackIndex]);
-                // floatParse(cells[floatValueIndex], Globalization.NumberStyle.Any, Globalization.CultureInfo.GetCultureInfo("en-US"), out floatValue)
-
-                _items.Add(new ItemData(i, title, description, price, stack));
-            }
+            _items = JsonConvert.DeserializeObject<List<ItemData>>(Resources.Load<TextAsset>("Database/ItemDatabase").ToString());
         }
     }
 }
